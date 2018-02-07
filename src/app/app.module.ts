89ce7391b70b11614cc9c6dd5f934eb8088027c7
renanreboredo <renanrreboredo@gmail.com>
2018-02-07 14:41:12 -0200

    Add message service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { MessagesComponent } from './messages/messages.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [HeroService],
+  providers: [HeroService, MessageService],
   bootstrap: [AppComponent]
 })
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -9,17 +10,17 @@ import { HeroService } from '../hero.service';
 })
 
 export class HeroesComponent implements OnInit {
-    heroes: Hero[];
     selectedHero: Hero;
+    heroes: Hero[];
 
     constructor(private heroService: HeroService) { }
 
     ngOnInit() { this.getHeroes(); }
 
+    onSelect(hero: Hero): void { this.selectedHero = hero; }
+
     getHeroes(): void {
         this.heroService.getHeroes()
             .subscribe(heroes => this.heroes = heroes);
     }
-
-    onSelect(hero: Hero): void { this.selectedHero = hero; }
 }
